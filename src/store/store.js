5ea54ba6895ec022bd4513e10b95c2864c5a3a8c
Frank Jogeleit <fj@move-elevator.de>
2017-08-24 16:07:21 +0200

    add redux toolbar support

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { createEpicMiddleware } from 'redux-observable';
 import todoReducer from './reducer/todoReducer'
 import todoEpics from './epics/todoEpics'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const epicMiddleware = createEpicMiddleware(todoEpics);
 
 export default createStore(
   todoReducer,
   undefined,
-  applyMiddleware(epicMiddleware)
+  composeEnhancers(applyMiddleware(epicMiddleware))
 )
