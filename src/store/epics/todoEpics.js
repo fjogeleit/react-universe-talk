4df1dfbd1f8ef59f0b807190a1a6bfb29252ef87
Frank Jogeleit <fj@move-elevator.de>
2017-08-24 16:59:27 +0200

    add comments

--- src/store/epics/todoEpics.js
+++ src/store/epics/todoEpics.js
@@ -3,15 +3,20 @@ import { combineEpics } from 'redux-observable'
 
 const fetchEpic = (action$) => action$
   .ofType('FETCH_TODOS')
+  // little delay to see the fetching notification
   .delay(2000)
   .flatMap(() =>
     Observable
+      // ajax get call to the local json-server api
       .ajax({ url: 'http://localhost:4000/todos', method: 'GET' })
+      // transform the result to the expected payload
       .map(result => result.response)
+      // dispatch success actions with the todos result
       .flatMap(payload => Observable.of(
         { type: 'SET_TODOS', payload },
         { type: 'FETCH_TODOS_FINISH' },
       ))
+      // dispatch an error action if the request call fails
       .catch(error => Observable.of({ type: 'FETCH_TODOS_ERROR', error }))
   )
 
@@ -19,12 +24,16 @@ const saveEpic = (action$) => action$
   .ofType('SAVE_TODO')
   .flatMap(action =>
     Observable
+    // execute an ajax request to persist the new todo
     .ajax({ url: 'http://localhost:4000/todos', method: 'POST', body: action.payload })
+    // transform the result to the expected payload
     .map(result => result.response)
+    // dispatch success actions with the todos result
     .flatMap(payload => Observable.of(
       { type: 'ADD_TODO', payload },
       { type: 'SAVE_TODO_FINISH' },
     ))
+    // dispatch an error action if the request call fails
     .catch(error => Observable.of({ type: 'SAVE_TODO_ERROR', error }))
   )
 
